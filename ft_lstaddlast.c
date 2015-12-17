09c2edf64e52225301c6cc75d042d0fefca6c80e
Augustin ROCHE <aroche@e1r13p9.42.fr>
2015-12-17 09:25:29 +0100

    Correction lstmap

--- ft_lstaddlast.c
+++ ft_lstaddlast.c
@@ -6,7 +6,7 @@
 /*   By: aroche <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/16 14:58:57 by aroche            #+#    #+#             */
-/*   Updated: 2015/12/16 15:31:45 by aroche           ###   ########.fr       */
+/*   Updated: 2015/12/17 09:04:29 by aroche           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,6 @@ void	ft_lstaddlast(t_list **alst, t_list *new)
 {
 	if ((*alst)->next == NULL)
 		(*alst)->next = new;
-	ft_lstaddlast(&((*alst)->next), new);
+	else
+		ft_lstaddlast(&((*alst)->next), new);
 }
--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,36 +6,27 @@
 /*   By: aroche <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/16 10:58:45 by aroche            #+#    #+#             */
-/*   Updated: 2015/12/16 16:24:25 by aroche           ###   ########.fr       */
+/*   Updated: 2015/12/17 09:24:31 by aroche           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
-t_list	*ft_lstsupernew(t_list *lst, t_list *(*f)(t_list *elem))
-{
-	t_list	*new;
-	t_list	*tmp;
-
-	tmp = ft_lstnew(NULL, 0);
-	tmp = f(lst);
-	new = ft_lstnew(tmp->content, tmp->content_size);
-	free(tmp);
-	return (new);
-}
-
+	
 t_list	*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem))
 {
 	t_list	*new;
+	t_list	*t;
 
 	if ((!lst) || (!f))
 		return (NULL);
-	new = ft_lstsupernew(lst, f);
+	t = f(lst);
+	new = ft_lstnew(t->content, t->content_size);
 	lst = lst->next;
 	while (lst)
 	{
-		ft_lstaddlast(&new, ft_lstsupernew(lst, f));
+		t = f(lst);
+		ft_lstaddlast(&new, t);
 		lst = lst->next;
 	}
-	return (lst);
+	return (new);
 }
